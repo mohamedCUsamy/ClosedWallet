0aa5d66556f4771121b9256104fbedae0ae019d5
Mohamed <mohamed.mohamed054@eng-st.cu.edu.eg>
2026-08-10 12:07:01 +0300

    Reapply "Merge pull request #1 from mohamedCUsamy/Empty-project"
    This reverts commit 34b8b3b7d998d3b380f2052d3b2cd752225aff7f.

---
+++ src/main/java/com/closedwallet/ClosedWalletApplication.java
@@ -0,0 +1,13 @@
+package com.closedwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosedWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosedWalletApplication.class, args);
+	}
+
+}
